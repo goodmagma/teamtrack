f4c9bc4d6ddf5847bd3570fdffd8f0094516fb24
Denis Torresan <denis.torresan@gmail.com>
2023-10-13 12:59:58 +0200

    configured vite for publish tabler assets

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -10,5 +12,54 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+		viteStaticCopy({
+            targets: [
+                {
+                    src: 'resources/fonts/*',
+                    dest: 'fonts'
+                },
+                {
+                    src: 'resources/images/*',
+                    dest: 'images'
+                },
+                {
+                    src: 'node_modules/@tabler/core/dist/js/tabler.min.js',
+                    dest: 'js'
+                },
+                {
+                    src: 'node_modules/@tabler/core/dist/css/tabler.min.css',
+                    dest: 'css'
+                },
+                {
+                    src: 'node_modules/@tabler/core/dist/css/tabler-flags.min.css',
+                    dest: 'css'
+                },
+                {
+                    src: 'node_modules/@tabler/core/dist/css/tabler-payments.min.css',
+                    dest: 'css'
+                },
+                {
+                    src: 'node_modules/@tabler/core/dist/css/tabler-vendors.min.css',
+                    dest: 'css'
+                },
+                {
+                    src: 'node_modules/tom-select/dist/js/tom-select.base.min.js',
+                    dest: 'js'
+                },
+                {
+                    src: 'node_modules/tom-select/dist/js/tom-select.base.min.js.map',
+                    dest: 'js'
+                },
+                {
+                    src: 'node_modules/@tabler/icons-webfont/*',
+                    dest: 'icons'
+                },
+            ]
+        })
     ],
+    resolve: {
+        alias: {
+          '~': path.resolve(__dirname, './node_modules'),
+        },
+    },
 });
